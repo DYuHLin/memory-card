b46909893d1c06c26831085b28e7bf6fa4746dd4
DYuHLin <damianver2000@gmail.com>
2023-04-12 10:15:01 +0200

    created a react application and removed boilerplate stuff, also created the header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Compnents/Header";
+import "./Style/style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Compnents/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+
+    return(
+        <div>
+            <div className='header'>
+                <div className="logo">
+                    <img src="./Assets/logo.png"></img>
+                </div>
+                <div className='title'>Gintama Memory Game</div>
+                <div className='link'>
+                    <a href='https://github.com/DYuHLin/cv-application'><i class='bx bxl-github'></i></a>              
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
